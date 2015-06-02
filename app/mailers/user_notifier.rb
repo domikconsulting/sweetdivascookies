62d68b11a85efd0c380dc6e355ab5074343890eb
Artem Nikitin <artem@domikconsulting.com>
2015-06-02 13:51:53 -0500

    Revert "Update user_notifier.rb"

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,6 +1,6 @@
 class UserNotifier < ApplicationMailer
-  default from: "noreply@sweetdivascookies.com"
-  default to: "orbitwhitex@gmail.com"
+  default from: ENV['SDC_EMAIL_REQ']
+  default to: ENV['SDC_EMAIL']
   default subject: 'Craving Those Cookies!'
 
   def req_email(name, email, content)
