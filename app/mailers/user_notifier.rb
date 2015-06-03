80573ad26eeaf4ac8fc7b2a2a541709e97625282
domikconsulting <artem@domikconsulting.com>
2015-06-02 21:08:42 -0500

    test_6_2_6

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,14 +1,12 @@
 class UserNotifier < ApplicationMailer
   default from: "noreply@sweetdivascookies.com"
-  default to: ENV['SDC_EMAIL']
-  default subject: 'Craving Those Cookies!'
 
   def req_email(name, email, content)
     @name = name
 	  @email = email
     @content = content
-   
-    mail( body: [@name, @email, @content]
-    )
+
+    mail( :to => ENV['SDC_EMAIL'],  :subject => 'Craving Those Cookies!')
+    
   end
 end
