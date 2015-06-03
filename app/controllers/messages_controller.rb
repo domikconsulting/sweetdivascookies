5a4ddaddb04425147e7ad6cb26fcb9ce5e649990
domikconsulting <artem@domikconsulting.com>
2015-06-02 20:42:02 -0500

    test_6_2_4

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,25 @@
 class MessagesController < ApplicationController
 
-#   def new
-#     @message = Message.new
-#   end
+  def index
+    @message = Message.new
+  end
 
-#   def create
-#     @message = Message.new(message_params)
+  def create
+    @message = Message.new(message_params)
     
-#     if @message.valid?
-#       UserNotifier.req_email(:name, :email, :content).deliver
-#       redirect_to contact_path, notice: "Your messages has been sent."
-#     else
-#       flash[:alert] = "An error occurred while delivering this message."
-#       render :new
-#     end
-#   end
+    if @message.valid?
+      UserNotifier.req_email(:name, :email, :content).deliver
+      redirect_to contact_path, notice: "Your messages has been sent."
+    else
+      flash[:alert] = "An error occurred while delivering this message."
+      render :index
+    end
+  end
 
-# private
+private
 
-#   def message_params
-#     params.require(:message).permit(:name, :email, :content)
-#   end
+  def message_params
+    params.require(:message).permit(:name, :email, :content)
+  end
 
 end
